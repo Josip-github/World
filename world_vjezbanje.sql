eac89592a351841008da762a6517e6537ef4039f
Josip-github <josipcota1@gmail.com>
2021-05-18 09:02:25 +0200

    promijeniti naziv grada
    Dok nisam koristio točku operator, Dbeaver je javljao grešku. Morao sam staviti: city.Name (tablica.stupac) kako bi se otklonile sve moguće greške.

--- world_vjezbanje.sql
+++ world_vjezbanje.sql
@@ -1,3 +1,6 @@
 #odaberi sve hrvatske gradove navedene u world
 select * from city where CountryCode ='HRV';
 
+#promijeni Donji Miholjac u Špičkovinu
+update city set city.Name = 'Špičkovina' where city.Name = 'Donji Miholjac';
+
