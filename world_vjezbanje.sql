7a7a4b1122710056b86fed7f80ed10ea99ccf2dc
Josip-github <josipcota1@gmail.com>
2021-05-18 09:20:19 +0200

    dodavanje par novih hrv gradova

--- world_vjezbanje.sql
+++ world_vjezbanje.sql
@@ -1,6 +1,13 @@
 #odaberi sve hrvatske gradove navedene u world
 select * from city where CountryCode ='HRV';
 
-#promijeni Donji Miholjac u Špičkovinu
-update city set city.Name = 'Špičkovina' where city.Name = 'Donji Miholjac';
+#dodaj nekoliko novih hrv gradova u tablicu city
+insert into city(Name,CountryCode,District,Population)
+values('Varaždin','HRV','Var',30000),
+('Bjelovar','HRV','Bje',18000),
+('Dubrovnik','HRV','Dub',35000),
+('Zadar','HRV','Zad',50000);
+
+
+
 
